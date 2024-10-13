d2f2dd8a0f442150849de0894e8bc7655df60f09
stanislav <stark.hateu@gmail.com>
2024-10-13 16:40:13 +0300

    all integration methods included

--- main.c
+++ main.c
@@ -45,7 +45,22 @@ int main(void) {
     } while (variant != 1 && variant != 2 && variant !=3 && variant != 4);
     system("cls");
 
-    printf("%lf = Result", integral_S);
+    switch (variant) {
+        case 1:
+            integral_S = calculate_integral_left_rectangles(left_boundary_a, right_boundary_b, intervals_n);
+            break;
+        case 2:
+            integral_S = calculate_integral_right_rectangles(left_boundary_a, right_boundary_b, intervals_n);
+            break;
+        case 3:
+            integral_S = calculate_integral_trapezium(left_boundary_a, right_boundary_b, intervals_n);
+            break;
+        case 4:
+            integral_S = calculate_integral_parabola(left_boundary_a, right_boundary_b, intervals_n);
+            break;
+    }
+
+    printf("%lf", integral_S);
     return 0;
 }
 
@@ -79,3 +94,48 @@ double calculate_integral_left_rectangles(double left_boundary_a, double right_b
 
     return sum * h;
 }
+double calculate_integral_right_rectangles(double left_boundary_a, double right_boundary_b, int intervals_n) {
+    // n - amount of rectangles, a - lower border of integrating, b - higher border of integrating
+    double h = (right_boundary_b - left_boundary_a) / intervals_n; // (b - a) / n - step
+    double sum = 0.0;
+
+    for (int i = 1; i <= intervals_n; i++) {
+        sum += function_for_integration(left_boundary_a + i * h); // X value in left border of integration
+    }
+
+    return sum * h;
+}
+double calculate_integral_trapezium(double left_boundary_a, double right_boundary_b, int intervals_n) {
+    // n - amount of rectangles, a - lower border of integrating, b - higher border of integrating
+    double h = (right_boundary_b - left_boundary_a) / intervals_n; // (b - a) / n - step
+    double sum = 0.0;
+
+    sum += (function_for_integration(left_boundary_a) + function_for_integration(right_boundary_b))/ 2; // add first and last values
+
+    for (int i = 1; i <= intervals_n; i++) {
+        sum += function_for_integration(left_boundary_a + i * h); // X value in left border of integration
+    }
+
+    return sum * h;
+}
+double calculate_integral_parabola(double left_boundary_a, double right_boundary_b, int intervals_n) {
+    if (intervals_n % 2 != 0) {
+        intervals_n++; // amount of intervals should be even
+    }
+
+    double h = (right_boundary_b - left_boundary_a) / intervals_n; // define step
+    double sum = function_for_integration(left_boundary_a) + function_for_integration(right_boundary_b); // adding first and last values
+
+    for (int i = 1; i < intervals_n; i += 2) {
+        sum += 4 * function_for_integration(left_boundary_a + i * h); //adding all odd values except first and last
+    }
+
+    for (int i = 2; i < intervals_n - 1; i += 2) {
+        sum += 2 * function_for_integration(left_boundary_a + i * h); //adding all even values except last and first
+    }
+
+    return (sum * h) / 3;
+}
+
+
+
